35a9c20f1771b57d5d0d4988729786da6e623666
Raimon <raimon.txollo@gmail.com>
2025-04-21 21:37:00 +0200

    Edit configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: { target: "esnext" },
+  test: {
+    root: "src",
+    globals: true,
+    reporters: ["verbose"],
+    environment: "jsdom",
+    setupFiles: ["./src/setupTest.ts"],
+    coverage: {
+      provider: "v8",
+      include: ["**/*.tsx", "**/*.ts"],
+      exclude: ["main.tsx", "**/*.d.ts", "**/types.ts", "**/App.tsx"],
+      reportsDirectory: "../coverage",
+    },
+  },
+});
