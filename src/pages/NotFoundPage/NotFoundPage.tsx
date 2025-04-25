94016facfdbce92c101cb458810992fca8e53323
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-25 19:15:49 +0200

    Add missing with & height properties

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -5,8 +5,10 @@ const NotFoundPage: React.FC = () => {
     <div className="not-found">
       <img
         className="not-found__image"
-        src="/images/notfound-small.webp"
+        src="/images/notfound-big.webp"
         alt="An empty plate with a sad face"
+        width={675}
+        height={729}
       />
       <span className="not-found__message">
         Oops! The page you're looking for doesn't exist
