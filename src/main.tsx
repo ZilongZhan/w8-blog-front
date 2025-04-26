87b35ad14d91df8879bc07083355a163be7884b4
Zilong Zhan <zolong.zhan@gmail.com>
2025-04-26 14:53:12 +0200

    Implement PostsContextProvider to application

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import AppRouter from "./router/AppRouter";
+import PostsContextProvider from "./post/context/PostsContextProvider";
 import "@fontsource-variable/roboto/index.css";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <PostsContextProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </PostsContextProvider>
   </StrictMode>,
 );
