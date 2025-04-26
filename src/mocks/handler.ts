2bbd93dd78f6d22f8e397dc93723c692378719b7
Raimon <raimon.txollo@gmail.com>
2025-04-26 16:27:29 +0200

    Add MSW handler and integrate it to Node.js process

---
+++ src/mocks/handler.ts
@@ -0,0 +1,17 @@
+import { http, HttpResponse } from "msw";
+import { microwaveRecipiesPostsDto } from "../post/dto/fixturesDto";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+if (!apiUrl) {
+  throw new Error("URL not found for the test");
+}
+
+export const handlers = [
+  http.get(`${apiUrl}/posts`, () => {
+    return HttpResponse.json({
+      posts: microwaveRecipiesPostsDto,
+      postsTotal: microwaveRecipiesPostsDto.length,
+    });
+  }),
+];
